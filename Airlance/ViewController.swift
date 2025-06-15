03ddbfc3302b1729f8f988dd3021028461642f60
Yura <yura@emercury.dev>
2025-06-15 12:23:06 +0300

    add xcode project

---
+++ Airlance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Airlance
+//
+//  Created by resoul on 15.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
